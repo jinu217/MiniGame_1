e00e265d3e3efe5f6254df4a748c7813144018f0
pm915145 <parkjud@naver.com>
2025-11-11 23:30:49 +0900

    feature/UI
    게임매니저 반영

--- Assets/HPMeasure.cs
+++ Assets/HPMeasure.cs
@@ -6,13 +6,12 @@ public class HPMeasure : MonoBehaviour
 {
     public Image hpFillImage;   // HP바 Fill 이미지
     public TMP_Text hpText;     // HP 수치 표시용 Text (TextMeshPro)
-
-    public float maxHP = 100f;
-    private float currentHP;
+    
+    public float maxHP = 100;
 
     void Start()
     {
-        currentHP = maxHP; // 시작 시 체력을 가득 채움
+        GameManager.gameManager.playerHp = maxHP; // 시작 시 체력을 가득 채움
         UpdateHPBar();
     }
 
@@ -26,10 +25,10 @@ public class HPMeasure : MonoBehaviour
     }
 
     // 체력 감소 처리
-    public void TakeDamage(float damage)
+    public void TakeDamage(int damage)
     {
-        currentHP -= damage;
-        if (currentHP < 0) currentHP = 0;
+        GameManager.gameManager.playerHp -= damage;
+        if (GameManager.gameManager.playerHp < 0) GameManager.gameManager.playerHp = 0;
         UpdateHPBar();
     }
 
@@ -37,10 +36,10 @@ public class HPMeasure : MonoBehaviour
     void UpdateHPBar()
     {
         if (hpFillImage != null)
-            hpFillImage.fillAmount = currentHP / maxHP;
+            hpFillImage.fillAmount = GameManager.gameManager.playerHp / maxHP;
 
         if (hpText != null)
-            hpText.text = Mathf.RoundToInt(currentHP) + " / " + Mathf.RoundToInt(maxHP);
+            hpText.text = GameManager.gameManager.playerHp + " / " + maxHP;
         // 예: 80 / 100
     }
 }
